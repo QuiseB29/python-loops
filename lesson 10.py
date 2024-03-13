d49350178c8ba77b1059188d57933cf3d7cb1ce6
QuiseB29 <bmarquise544@gmail.com>
2024-03-13 03:05:09 -0500

    Last assignment

---
+++ lesson 10.py
@@ -0,0 +1,23 @@
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+selected_genres = genres[1:4]  # Slice from index 1 (inclusive) to index 4 (exclusive)
+
+for index, genre in enumerate(selected_genres, start=2):
+    print(f"Track {index}: Now playing {genre}")
+
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+new_list = ["Country", "R&B", "Techno", "Soul"]
+
+new_list = [genre + " Music" for genre in genres]
+
+print(new_list)
+
+#cOUNTDOWN
+
+Timer = 10
+for i in range (10, 0,-1):
+    print(i)
+print("beat drop now")
+   
+     
+
